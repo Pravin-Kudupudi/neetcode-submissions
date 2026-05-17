711c010a38d810f50324c01e4eb85cc263c87c3a
Pravin-Kudupudi <135609889+Pravin-Kudupudi@users.noreply.github.com>
2026-05-17 09:34:25 +0530

    Bulk sync: 11 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        mapS, mapT = {}, {}
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            mapS[s[i]] = mapS.get(s[i], 0) + 1
+            mapT[t[i]] = mapT.get(t[i], 0) + 1
+        
+        return mapS == mapT
